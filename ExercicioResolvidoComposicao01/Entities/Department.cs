0b258add74ed5e97e08f855e144281db3fcbe944
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:25:08 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioResolvidoComposicao01/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace ExercicioResolvidoComposicao01.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ ExercicioResolvidoComposicao01/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ExercicioResolvidoComposicao01.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
